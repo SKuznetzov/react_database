8e05545fee933a3893b0c8aec41b8fdcfbb78f4a
SKuznetzov <s-kuznecov100@rambler.ru>
2022-03-09 21:04:38 +0300

    add component FollowersColumn

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 // import React, { Component } from 'react'
-
+import FollowersColumn from '../components/FollowersColumn'
 const Home = () => {
         return (
             <div>
                 <h1>Home</h1>
+                <FollowersColumn />
             </div>
         )
     }
