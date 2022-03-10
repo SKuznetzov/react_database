742b1bc86937ae019a1cdd963547c6bbf8b51dc5
SKuznetzov <s-kuznecov100@rambler.ru>
2022-03-10 22:10:55 +0300

    astraDatabaseId

--- functions/addData.js
+++ functions/addData.js
@@ -1,1 +1,12 @@
 const { createClient } = require("@astrajs/collections")
+
+
+const astraClient = await createClient({
+    astraDatabaseId: process.env.ASTRA_DB_ID,
+    astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+    username: process.env.ASTRA_DB_USERNAME,
+    password: process.env.ASTRA_DB_PASSWORD,
+
+
+
+
