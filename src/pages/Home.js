e9aa09909daa9f75bb22f2af74f10f93bbb2435f
SKuznetzov <s-kuznecov100@rambler.ru>
2022-03-14 21:52:28 +0300

    formations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,74 +1,72 @@
-import React, { useState, useEffect } from 'react'
-import FollowersColumn from '../components/FollowersColumn'
-import Card from '../components/Card'
-import MiniCard from '../components/MiniCard'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import FollowersColumn from "../components/FollowersColumn";
+import Card from "../components/Card";
+import MiniCard from "../components/MiniCard";
+import axios from "axios";
 
-import './Home.css'
+import "./Home.css";
 const Home = () => {
-    const [users, setUsers] = useState(null)
-  let descendingUsers
-  let topFiveNotFollowing
-  let topFiveFollowing
+  const [users, setUsers] = useState(null);
+  let descendingUsers;
+  let topFiveNotFollowing;
+  let topFiveFollowing;
 
+  const addData = async () => {
+    await axios.post("/.netlify/functions/addData");
+  };
+  const fetchData = async () => {
+    const results = await axios.get("/.netlify/functions/posts");
+    console.log(results.data);
+    setUsers(results.data);
+  };
+  useEffect(() => {
+    addData();
+    fetchData();
+  }, []);
+  if (users) {
+    descendingUsers = users.sort((a, b) => (a.id < b.id ? 1 : -1));
+    const following = users.filter((user) => user.is_followed === true);
+    const descendingFollowing = following.sort((a, b) =>
+      a.likes < b.likes ? 1 : -1
+    );
+    topFiveFollowing = descendingFollowing.slice(0, 5);
 
-    const addData = async () => {
-        await axios.post('/.netlify/functions/addData')
-      }
-      const fetchData = async () => {
-        const results = await axios.get('/.netlify/functions/posts')
-        console.log(results.data)
-        setUsers(results.data)
-      }
-      useEffect(() => {
-        addData()
-        fetchData()
-      }, [])
-      if (users) {
-        descendingUsers = users.sort((a, b) => a.id < b.id ? 1 : -1)
-        const following = users.filter(user => user.is_followed === true)
-        const descendingFollowing = following.sort((a, b) => a.likes < b.likes ? 1 : -1)
-        topFiveFollowing = descendingFollowing.slice(0, 5)
-
-        const notFollowing = users.filter((user) => user.is_followed === false)
-        const descendingNotFollowing = notFollowing.sort((a, b) => a.likes < b.likes ? 1 : -1)
-        topFiveNotFollowing = descendingNotFollowing.slice(0, 5)
-      }
-      return (
-            <>
-    {descendingUsers && (
-
-            <div className="container">
-                <FollowersColumn users={topFiveFollowing}/>
-                <div className="feed">
-                {descendingUsers.map((descendingUser, index) => (
-            <Card
-              key={index}
-              user={descendingUser}
-              toggleFollow={userToToggle => setUserToToggle(userToToggle)}
-            />
-          ))}     
-                    <h1>Home</h1>
-                </div>
-                <div className="suggested-box">
-                    <div className="section">
-                        <div className="suggested">
-                            <h2 className="bold">Suggested accounts</h2>
-                            <div className="break" />
-                            {topFiveNotFollowing.map((notFollowingUser, index) => (
-                <MiniCard 
-                  key={index} 
-                  user={notFollowingUser}
-		  	        />)
-		  	      )}
-
-                        </div>
-                      </div>
-                </div>
+    const notFollowing = users.filter((user) => user.is_followed === false);
+    const descendingNotFollowing = notFollowing.sort((a, b) =>
+      a.likes < b.likes ? 1 : -1
+    );
+    topFiveNotFollowing = descendingNotFollowing.slice(0, 5);
+  }
+  return (
+    <>
+      {descendingUsers && (
+        <div className="container">
+          <FollowersColumn users={topFiveFollowing} />
+          <div className="feed">
+            {descendingUsers.map((descendingUser, index) => (
+              <Card
+                key={index}
+                user={descendingUser}
+                toggleFollow={(userToToggle) => setUserToToggle(userToToggle)}
+              />
+            ))}
+            <h1>Home</h1>
+          </div>
+          <div className="suggested-box">
+            <div className="section">
+              <div className="suggested">
+                <h2 className="bold">Suggested accounts</h2>
+                <div className="break" />
+                {topFiveNotFollowing.map((notFollowingUser, index) => (
+                  <MiniCard key={index} user={notFollowingUser} />
+                ))}
+              </div>
             </div>
-    )}
-            </>
-        )
-    }
-    
-export default Home
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Home;
