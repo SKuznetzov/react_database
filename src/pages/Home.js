8e3eca43199f95f048845a8740426bb0b708d6f7
SKuznetzov <s-kuznecov100@rambler.ru>
2022-03-16 09:30:16 +0300

    axios put

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,6 +12,7 @@ const Home = () => {
   let topFiveNotFollowing;
   let topFiveFollowing;
 
+  console.log('userToToggle',userToToggle)
   const addData = async () => {
     await axios.post("/.netlify/functions/addData");
   };
@@ -20,6 +21,17 @@ const Home = () => {
     console.log(results.data);
     setUsers(results.data);
   };
+  if (userToToggle) {
+    const newValue = userToToggle.is_followed ? false : true
+    const data = {is_followed: newValue}
+  
+     axios.put('/.netlify/functions/edit', {userId: userToToggle.id, data: data })
+      .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.error('error:' + err))
+      .then(() => fetchData())
+        setUserToToggle(null)
+    }
   useEffect(() => {
     addData();
     fetchData();
